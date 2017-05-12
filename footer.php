f3671e9a0b29d85ca0f5def548df325543fb7760
Alpesh <alpeshspce20@gmail.com>
2017-05-12 21:05:07 +0530

    chnges

--- footer.php
+++ footer.php
@@ -14,6 +14,7 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+<script type="text/javascript" src="assets/js/jquery.touchSwipe.min.js"></script>
 <script type="text/javascript" src="http://static.myrealestatesite.co/js/tablesorter.min.js"></script>
 <script type="text/javascript" src="http://static.myrealestatesite.co/js/acc.js"></script>
 
